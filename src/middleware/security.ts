133182b4fa7a3543b5288dfc17e6712aa524d006
Sandip <sandipghosh0312@gmail.com>
2026-01-20 14:12:05 +0530

    Implement class details

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -23,7 +23,7 @@ const securityMiddleware = async (request: Request, response: Response, next: Ne
                 message = 'User request limit exceeded (10 per minute)';
                 break;
             default:
-                limit = 5;
+                limit = 30;
                 message = 'Guest request limit exceeded (5 per minute). Sign up for higher limits';
         }
 
